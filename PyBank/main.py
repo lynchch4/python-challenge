809fc3603215d7d2df76e4617b88e9cd0f7cf06d
lynchch4 <lynchch4@gmail.com>
2023-01-14 04:43:40 -0500

    Added Main Code

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,19 +1,87 @@
 import os
 import csv
 
-# Header
+# print header
 print("")
 print("Financial analysis")
 print("")
 print("-----------------------------------------")
 print("") 
+ 
+# assign variables
+count = 0
+total = 0
+max = 0
+min = 0
+previous = 0
+change = 0
+changetotal = 0
+average = 0
+count2 = 0
 
-    
 # set path for files
+path_name = '/Users/Chris/OneDrive/Documents/python-challenge/PyBank/Resources'
 
+csvpath = os.path.join("", "Resources", "budget_data.csv")
 
-csvpath = os.path.join("..", "Resources", "budget_data.csv")
+out_name = '/Users/Chris/OneDrive/Documents/python-challenge/PyBank/analysis'
 
-
+out_path = os.path.join("", "analysis", "bank_analysis.txt")
+    
+# open CSV file as read
 with open(csvpath, encoding='utf') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
+    
+    # skip header
+    next(csvreader, None)
+
+    # for loop
+    for row in csvreader:
+
+       # count of total months
+       count += 1
+       
+       # sum of monthly investments
+       total += int(row[1])
+
+       # skip results where there was not previous change in profit/loss
+       if previous == 0:
+        change = 0
+       elif previous != 0:
+        change = int(row[1]) - previous
+        count2 += 1
+
+       # sum of change in variance
+       changetotal += change 
+
+       # assign variable for previous row profit/loss
+       previous = int(row[1])
+     
+       # find min/max date and amount for change in profit/loss
+       if int(change) > int(max):
+        max = int(change)
+        datemax = row[0]
+       
+       if int(change) < int(min):
+        min = int(change)
+        datemin = row[0]
+    
+    average = round(changetotal / (count2), 2)
+
+    # print results
+    print("Total Months: " + str(count))
+    print("")
+    print("Total: $" + str(total))
+    print("")
+    print("Average Change: $" + str(average))
+    print("")
+    print("Greatest Increase in Profits: " + str(datemax) + " ($" + str(max) + ")")
+    print("")
+    print("Greatest Decrease in Profits: " + str(datemin) + " ($" + str(min) + ")")
+    print("")
+ 
+# produce textfile for results
+with open(out_path, 'w') as textfile:
+    textfile.writelines(["\n", "Financial Analysis \n", "\n", "---------------------------------------------------- \n", "\n", "Total Months: " + str(count) + "\n",
+    "\n", "Total: $" + str(total) + "\n", "\n", "Average Change: $" + str(average) + "\n", "\n", "Greatest Increase in Profits: " + str(datemax) + " ($" + str(max) + ")" + "\n", "\n",
+    "Greatest Decrease in Profits: " + str(datemin) + " ($" + str(min) + ") \n"])
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,20 +1,83 @@
 import os
 import csv
 
-# Header
+
+# print header
 print("")
 print("Election Results")
 print("")
 print("--------------------------------")
 print("")
     
+# assign variables
+total = 0
+charles = 0
+diana = 0
+raymon = 0
+
 # set path for files
+path_name = '/Users/Chris/OneDrive/Documents/python-challenge/PyPoll/Resources'
 
+csvpath = os.path.join("", "Resources", "election_data.csv")
 
-csvpath = os.path.join("..", "Resources", "election_data.csv")
+out_name = '/Users/Chris/OneDrive/Documents/python-challenge/PyPoll/analysis'
 
+out_path = os.path.join("", "analysis", "election_analysis.txt")
+
+# open CSV file as read
 with open(csvpath, encoding='utf') as csvfile:
-    csvreader=csv.reader(csvfile, delimiter=",")
+    csvreader = csv.reader(csvfile, delimiter=",")
 
-print(os.getcwd())
+    # skip header
+    next(csvreader, None)
 
+    # for loop
+    for row in csvreader:
+        
+        # add a counter
+        total += 1
+
+        # assign a counter for each candidates
+        if row[2] == "Charles Casper Stockham":
+            charles += 1
+        elif row[2] == "Diana DeGette":
+            diana += 1
+        elif row[2] == "Raymon Anthony Doane":
+            raymon += 1
+
+        # assign variables for percentage of votes and round to 3 decimals
+        cper = round(charles/total*100, 3)
+        dper = round(diana/total*100, 3)
+        rper = round(raymon/total*100, 3)
+
+        # if statement for which candidate had most votes
+        if charles > diana and charles > raymon:
+            winner = "Charles Casper Stockham"
+        elif diana > charles and diana > raymon:
+            winner = "Diana DeGette"
+        elif raymon > diana and raymon > charles:
+            winner = "Raymon Anthony Doane"
+    
+    # print results
+    print("Total Votes: " + str(total))
+    print("")
+    print("--------------------------------")
+    print("")
+    print("Charles Casper Stockham: " + str(cper) + "% (" + str(charles) + ")")
+    print("")
+    print("Diana DeGette: " + str(dper) + "% (" + str(diana) + ")")
+    print("")
+    print("Raymon Anthony Doane: " + str(rper) + "% (" + str(raymon) + ")")
+    print("")
+    print("--------------------------------")
+    print("")
+    print("Winner: " + str(winner))
+    print("")
+    print("--------------------------------")
+
+# produce textfile for results
+with open(out_path, 'w') as textfile:
+    textfile.writelines(["\n", "Election Results \n", "\n", "---------------------------------------------------- \n", "\n", "Total vote: " + str(total) + "\n",
+    "\n", "---------------------------------------------------- \n", "\n", "Charles Casper Stockham: " + str(cper) + "% (" + str(charles) + ")" + "\n", "\n", 
+    "Diana DeGette: " + str(dper) + "% (" + str(diana) + ")" + "\n", "\n", "Raymon Anthony Doane: " + str(rper) + "% (" + str(raymon) + ") \n", "\n", 
+    "---------------------------------------------------- \n", "\n", "Winner: " + str(winner) + "\n", "\n" "---------------------------------------------------- \n"])
